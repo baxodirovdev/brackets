ea569f0515c17f9088fdd9eb47574471c93bf380
baxodirovdev <76782350+baxodirovdev@users.noreply.github.com>
2021-03-15 11:41:41 +0500

    fix: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ const parsedBracketConfig = bracketsConfig.map((r) => {
+        return `${r[0]}${r[1]}`;
+    });
+
+    let prevLength = str.length;
+
+    while (str !== "") {
+        parsedBracketConfig.forEach((r) => {
+            str = str.replace(r, "");
+        });
+
+        if(prevLength === str.length){
+          return false
+        }
+
+        prevLength = str.length
+    }
+    return true
 }
